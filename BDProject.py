6b6097b57a0884173054748854ce3965859b7bed
Jorren Bosga <jorren.bosga@hotmail.com>
2015-03-11 14:59:08 +0100

    Initial project file.

---
+++ BDProject.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+
+data = pd.io.stata.read_stata("/Users/jorrenbosga/Desktop/AUC/Masterclass/GSS2012.dta")
