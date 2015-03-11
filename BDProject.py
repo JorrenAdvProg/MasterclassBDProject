c81c962b66579a02a8e22d157a5c3dea335e643b
Matthijs Meijers <Matthijsmeijers@wcw-stud-145-109-1-202.wireless.uva.nl>
2015-03-11 15:48:04 +0100

    figure

--- BDProject.py
+++ BDProject.py
@@ -71,19 +71,19 @@ for income in data.income:
     elif income == '$25000 OR MORE':
         incomegroups[12] += 1
         
-        
-plt.subplot(4,1,1)
+plt.figure(1,(5,6))        
+plt.subplot(3,1,1)
 plt.bar(range(10,90), agecounts)
 plt.xlabel("age")
 plt.ylabel("frequency")
-plt.subplot(4,1,2)
+plt.subplot(3,1,2)
 plt.bar([1,2], [male, female])
 plt.xticks([1.5,2.5], ("Male", "Female"))
 plt.ylabel("frequency")
-plt.subplot(4,1,3)
+plt.subplot(3,1,3)
 plt.bar([1,2,3], [RACE['white'], RACE['black'], RACE['other']], 0.5)
 plt.xticks([1.25,2.25,3.25], ('White', 'Black', 'Other'))      
 plt.ylabel("frequency")
-plt.subplot(4,1,4)
-plt.bar(range(1,14), incomegroups, 1)
+plt.figure(2,(9,5))
+plt.bar(range(0,13,1), incomegroups, 1)
 plt.xticks([0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, 11.5, 12.5], ('nan','<1k','1k-3k','3k-4k','4k-5k','5k-6k','6k-7k','7k-8k','8k-10k','10k-15k','15k-20k','20k-25k','>25k'))
