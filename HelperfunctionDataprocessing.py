c45c022954cc01279ea65eb236b8c46b75f323b6
Matthijs Meijers <Matthijsmeijers@MacBook-Air-van-Matthijs.local>
2015-03-19 20:57:32 +0100

    religion plot

--- HelperfunctionDataprocessing.py
+++ HelperfunctionDataprocessing.py
@@ -20,7 +20,64 @@ def NumberofBelievers(Religionlist, belief):
             year += 1
             
     return Believers
+    
+def listofyears():
+    year = 1972
+    listofyears = []
+    for i in range(28):
+        if year == 1979 or year == 1981 or year == 1984 or year == 1986 or year ==1992:
+            year += 1
+        listofyears.append(year)
+        year += 1
+        if year > 1994:
+            year += 1
+    return listofyears
         
+        
+        
+
+def Loopoveryears(Religionlist, belief, others):
+    year = 1972
+    Distribution = []
+    for i in range(28):
+        if year == 1979 or year == 1981 or year == 1984 or year == 1986 or year ==1992:
+            year += 1
+        population = 0
+        believers = 0
+        for believer in Religionlist[str(year)]:
+            if believer == belief:
+                believers += 1
+            if believer != 0:
+                population += 1
+            
+            if belief == 'other':
+                if believer in others:
+                    believers +=1
+                    
+                        
+        
+        Distribution.append(float(believers)/float(population))
+        
+        year += 1
+        if year > 1994:
+            year += 1
+            
+    return Distribution
+                
+def Religionplots(Religionlist, beliefs, listofyears, others):
+    Numberofbeliefs = len(beliefs)
+    for i, belief in enumerate(beliefs):
+        Distribution = Loopoveryears(Religionlist, belief, others)
+        plt.figure(1,(8,25))
+        plt.subplot(Numberofbeliefs,1,i+1)
+        plt.plot(listofyears, Distribution)
+        plt.ylabel("Probability")
+        plt.xlabel("Year")
+        plt.title(belief)
+        plt.tight_layout()
+        
+        
+    
         
 def plotrates(listoflist, names):
     
